e3232ae06672f4d23b4ea09f95879f215fc3a030
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-05-13 23:05:18 +0530

    day - 9

---
+++ 2623. Memoize.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (key in cache) {
+            return cache[key];
+        }
+
+        const ans = fn.apply(this, args);
+        cache[key] = ans;
+        return ans;
+    }
+}
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
